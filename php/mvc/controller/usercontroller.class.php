4a07cb087450f7d1558b235c5b94ed50b512e7a5
Ammar <ammaryasir03062000@gmail.com>
2020-05-07 15:31:10 +0800

    UserController - Registration v1

--- php/mvc/controller/usercontroller.class.php
+++ php/mvc/controller/usercontroller.class.php
@@ -2,4 +2,8 @@
 
 class UserController extends UserModel {
     
+    public function Register($username, $password, $repassword, $email) {
+        return $this->createUser($username, $password, $repassword, $email);
+    }
+
 }
