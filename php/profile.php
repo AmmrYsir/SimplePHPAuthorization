8a66bf23a674a81f69d683cb418aa0ce751e3643
Ammar <ammaryasir03062000@gmail.com>
2020-05-10 08:41:35 +0800

    Add profile.css

--- php/profile.php
+++ php/profile.php
@@ -1,11 +1,12 @@
 <?php
     include 'includes/autoload.inc.php';
     $style = '<link rel="stylesheet" href="../css/profile.css">';
+    $title = 'Your name | Profile'
 
 ?>
 
 <?php ob_start() ?>
-
+    
 <?php $content = ob_get_clean(); ?>
 
 <?php include '../template.php';
