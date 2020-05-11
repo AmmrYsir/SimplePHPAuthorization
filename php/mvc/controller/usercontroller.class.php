ce4edbc86e1482c022f6684932651a6360d8481c
Ammar <ammaryasir03062000@gmail.com>
2020-05-11 14:14:02 +0800

    code refactor

--- php/mvc/controller/usercontroller.class.php
+++ php/mvc/controller/usercontroller.class.php
@@ -3,7 +3,22 @@
 class UserController extends UserModel {
     
     public function Register($username, $password, $repassword, $email) {
-        return $this->createUser($username, $password, $repassword, $email);
+        if(empty($username) || empty($password) || empty($repassword) || empty($email)) {
+            $Empty = true;
+        }
+        else if ($this->checkUsernameValid($username) === false) {
+            $usernameNoValid = true;
+        }
+        else if ($password !== $repassword){
+            $passwordRepeatNoValid = true;
+        }
+        else if (filter_var($email, FILTER_VALIDATE_EMAIL) && $this->checkEmailValid($email) === false){
+            $emailNoValid = true;
+        }
+        else {
+            $this->createUser($username, $password, $repassword, $email);
+            return true;
+        }
     }
 
     public function Login($username, $password) {
